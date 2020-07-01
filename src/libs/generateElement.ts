6044084653129f41d348bad9e3c9951c29e55536
cmlee <bal.dongdongdong@gmail.com>
2020-07-01 23:11:38 +0900

    update addRootElement lib

--- src/libs/generateElement.ts
+++ src/libs/generateElement.ts
@@ -1,4 +1,6 @@
 export const addRootElement = (id: string) => {
+  const exist = document.getElementById(id);
+  if (exist) return;
   const element = document.createElement('div');
   element.setAttribute('id', id);
   document.body.appendChild(element);
