71ff5be3185eb309fbe753c81579520274a4e594
cmlee <bal.dongdongdong@gmail.com>
2020-07-09 23:54:36 +0900

    add drag feature

--- src/applications/Tetris/Tetris.tsx
+++ src/applications/Tetris/Tetris.tsx
@@ -2,6 +2,14 @@ import React, { ReactElement } from 'react';
 import Icon from 'components/Icon';
 import TetrisIcon from 'assets/images/icons/icon-tetris.png';
 import { Window, WindowHeader, WindowContent, Button } from 'react95';
+import Draggable from 'react-draggable';
+import styled from 'styled-components';
+
+const WindowHeaderContainer = styled(WindowHeader)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 function Tetris(): ReactElement {
   const run = (): void => {
@@ -11,28 +19,24 @@ function Tetris(): ReactElement {
   return (
     <>
       <Icon icon={TetrisIcon} name="tetris" handleDoubleClick={run} />
-      <Window style={{ width: 400 }}>
-        <WindowHeader
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <span>react95.exe</span>
-          <Button style={{ marginRight: '-6px', marginTop: '1px' }} size={'sm'} square>
-            <span style={{ fontWeight: 'bold', transform: 'translateY(-1px)' }}>x</span>
-          </Button>
-        </WindowHeader>
-        <WindowContent>
-          <ul>
-            <li>something here</li>
-            <li>something here</li>
-            <li>something here</li>
-            <li>something here</li>
-          </ul>
-        </WindowContent>
-      </Window>
+      <Draggable handle={`.${WindowHeaderContainer.styledComponentId}`}>
+        <Window style={{ width: 400 }}>
+          <WindowHeaderContainer>
+            <span>react95.exe</span>
+            <Button style={{ marginRight: '-6px', marginTop: '1px' }} size={'sm'} square>
+              <span style={{ fontWeight: 'bold', transform: 'translateY(-1px)' }}>x</span>
+            </Button>
+          </WindowHeaderContainer>
+          <WindowContent>
+            <ul>
+              <li>something here</li>
+              <li>something here</li>
+              <li>something here</li>
+              <li>something here</li>
+            </ul>
+          </WindowContent>
+        </Window>
+      </Draggable>
     </>
   );
 }
