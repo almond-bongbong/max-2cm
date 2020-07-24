49131e6f488e1ec0af47f800c0657f08e691cf30
cmlee <bal.dongdongdong@gmail.com>
2020-07-24 23:43:08 +0900

    Updates

---
+++ precache-manifest.d5b6b620a53d2604cb18c5d3f02052c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4180d780414e824a9e943979f2a7c1",
+    "url": "/max-2cm/index.html"
+  },
+  {
+    "revision": "125b9c5079deb2bc1519",
+    "url": "/max-2cm/static/js/2.99af8e2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/max-2cm/static/js/2.99af8e2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e18be319f2c04d651347",
+    "url": "/max-2cm/static/js/main.54cb37c9.chunk.js"
+  },
+  {
+    "revision": "fb9f416afb0c2f3000da",
+    "url": "/max-2cm/static/js/runtime-main.7e244f60.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/max-2cm/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/max-2cm/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
